1baec664d89dce9dbd77914a0fa0e9a54a3e2db4
cerulean-revision-6689 <jordanjennings12@gmail.com>
2020-11-13 01:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function divide(){
 function add(a, b){
   return a + b
 }
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
